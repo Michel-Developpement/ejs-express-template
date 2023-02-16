cbf8999bb3aaa5c0d0f5ff9aa214da018e5168ac
antonin michel <a.copiercoller@gmail.com>
2023-02-16 09:32:01 +0100

    Ajout de boucles

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const ejs = require("ejs");
 const app = express();
 const port = 8080;
 
+const articles = [
+  {
+    title: 'Apprendre HTML CSS et JavaScript',
+    category: 'Développement Web',
+  },
+  {
+    title: 'Apprendre Node.js',
+    category: 'Développement Web',
+  },
+  {
+    title: 'Apprendre React',
+    category: 'Développement Web',
+  },
+  {
+    title: 'Apprendre TypeScript',
+    category: 'Développement Web',
+  },
+]
+
+
 app.set("view engine", "ejs");
 app.use("/static", express.static("public"));
 app.get("/", (req, res) => {
@@ -25,6 +45,9 @@ app.get('/hello/:name', (req, res) => {
     name: req.params.name,
   }
   res.render('hello', data);
+})
+app.get('/posts', (req, res) => {
+  res.render('post-list', { posts: articles });
 })
 app.listen(port, () => {
   console.log(`Server Start to http://localhost/:${port} !`)
